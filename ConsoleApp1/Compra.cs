ac3c470bcf65d5806920e721f32e484133dbe3b4
Dani-Hrdz <105619891+Dani-Hrdz@users.noreply.github.com>
2022-06-13 11:02:31 -0700

    programa 1

---
+++ ConsoleApp1/Compra.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class Compra
+    {
+        string producto;
+        int cantidad, precio;
+        StreamWriter sw = null;
+
+        public Compra(string producto, int cantidad, int precio)
+        {
+            this.cantidad = cantidad;
+            this.producto = producto;
+            this.precio = precio;
+            
+        }
+        public void crearArchivo()
+        { 
+
+            try
+            {
+                sw = new StreamWriter("Compras.txt", true);
+                sw.Write("\nProducto: " + producto);
+                sw.Write("\nCantidad de compra: " + cantidad);
+                sw.Write("\nPrecio: {0:C}", precio);
+                sw.Close();
+                    
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("\nError: " + e.Message);
+                Console.WriteLine("\nRuta: " + e.StackTrace);
+            } 
+        }
+        public void Desplegar()
+        {
+            Console.Clear();
+            Console.WriteLine("Producto: " + producto);
+            Console.WriteLine("Cantidad a comprar: " + cantidad);
+            Console.WriteLine("Precio: {0:C}", precio);
+
+        }
+    }
+    
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string producto;
+            int precio, cantidad;
+
+            try
+            {
+                Console.Write("Nombre del producto: ");
+                producto = Console.ReadLine();
+                Console.Write("Precio del producto: ");
+                precio = int.Parse(Console.ReadLine());
+                Console.Write("Cantidad a comprar: ");
+                cantidad = int.Parse(Console.ReadLine());
+                Compra c1 = new Compra(producto, cantidad, precio);
+                c1.crearArchivo();
+                c1.Desplegar();
+                Console.ReadKey();
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine("\nError: " + e.Message);
+                Console.WriteLine("\nRuta: " + e.StackTrace);
+                Console.ReadKey();
+            }
+        }
+    }
+}
